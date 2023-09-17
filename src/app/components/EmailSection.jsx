ce6b6a1d2a4727e53f84dd560b4d83fc7b48d8c6
Hsen-123 <hazimehussein01@gmail.com>
2023-09-17 17:23:04 +0300

    first commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -8,8 +8,7 @@ const EmailSection = () => {
   return (
     <section
       id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
-    >
+      className="grid z-0 md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
@@ -35,8 +34,7 @@ const EmailSection = () => {
           <div className="mb-6">
             <label
               htmlFor="email"
-              className="text-white block mb-2 text-sm font-medium"
-            >
+              className="text-white block mb-2 text-sm font-medium">
               Your email
             </label>
             <input
@@ -50,8 +48,7 @@ const EmailSection = () => {
           <div className="mb-6">
             <label
               htmlFor="subject"
-              className="text-white block text-sm mb-2 font-medium"
-            >
+              className="text-white block text-sm mb-2 font-medium">
               Subject
             </label>
             <input
@@ -65,8 +62,7 @@ const EmailSection = () => {
           <div className="mb-6">
             <label
               htmlFor="message"
-              className="text-white block text-sm mb-2 font-medium"
-            >
+              className="text-white block text-sm mb-2 font-medium">
               Message
             </label>
             <textarea
@@ -78,8 +74,7 @@ const EmailSection = () => {
           </div>
           <button
             type="submit"
-            className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
-          >
+            className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full">
             Send Message
           </button>
         </form>
